1a24816930231432aff04d1f3ac08215873af692
Pia <33084683+piax654e@users.noreply.github.com>
2017-12-16 15:22:45 +0100

    Functions.php
    Filen har fået oprettet en mappe (inc) til placering og kodekommentarer er rettet.

--- inc/functions.php
+++ inc/functions.php
@@ -15,7 +15,8 @@ function GetJsonData() {
     return $json->pages;
 }
 
-//Og indholdet afkodes med _decode:
+//Denne function skaber menuen med et "foreach", som løber alle sider igennem (via $page som er alias for $pages)
+//og finder valuen "menuTitle", som den skriver ud til skærmen med "echo".
 function Menu() {
     $pages = GetJsonData();
     foreach($pages as $page) {
@@ -23,19 +24,22 @@ function Menu() {
     }
 }
 
-//Her laves en funktion, som via GET genererer de enkelte sider. "page" er et alias for "pages":
+//Her er funktionen, som via GET genererer de enkelte sider:
 function GetCurrentPage() {
     $currentPageId = STARTPAGE;
     if(isset($_GET["page"])) {
         $currentPageId = $_GET["page"];
     }
 
+//Med "if" angives det, at den kun skal finde én side ad gangen, nemlig dén med "$currentPageId":
     $pages = GetJsonData();
     foreach($pages as $page) {
         if($page->id == $currentPageId) {
             $currentPage = $page;
         }
     }
+
+//Og til sidst kommandoen, som får den aktive side vist:
     return $currentPage;
 }
 ?>
